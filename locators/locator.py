643068759b734ef4ec8b6aecc612623a08dccd12
addressdecim <143195522+addressdecim@users.noreply.github.com>
2024-03-01 21:09:55 +0300

    locator.py

--- locators/locator.py
+++ locators/locator.py
@@ -4,29 +4,3 @@ password = "//input[@id='password']"
 button_login = "//input[@id='login-button']"
 txt_eror = "//h3[@data-test='error']"
 button_eror = "//button[@class='error-button']"
-
-# MainPage
-sort_product = "//select[@class='product_sort_container']"
-sort_price = "//option[@value='hilo']"
-main_name_product = "//div[@class='inventory_item_name']"
-main_price_product = "//div[@class='inventory_item_price'][2]"
-button_add_card = "//button[@class='btn btn_primary btn_small btn_inventory ']"
-button_card = "//div[@id='shopping_cart_container']"
-
-# CardPage
-card_name_product = "//div[@class='inventory_item_name']"
-card_price_product = "//div[@class='inventory_item_price']"
-button_checkout = "//button[@class='btn btn_action btn_medium checkout_button ']"
-
-# CheckoutPage
-first_name = "//input[@placeholder='First Name']"
-last_name = "//input[@placeholder='Last Name']"
-postal_code = "//input[@placeholder='Zip/Postal Code']"
-button_continue = "//input[@type='submit']"
-
-# FinishPage
-button_finish = "//button[@class='btn btn_action btn_medium cart_button']"
-
-# EndPage
-button_burger = "//div[@class='bm-burger-button']"
-button_logout = "//a[@id='logout_sidebar_link']"
